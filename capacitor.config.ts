33efd824d558f4a7563ef93979287c13a56be972
Javier Aguilarte <javier20450@gmail.com>
2025-05-08 16:52:21 -0500

    subida inicial al git

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ecandle.app',
+  appName: 'ecandle',
+  webDir: 'www'
+};
+
+export default config;
