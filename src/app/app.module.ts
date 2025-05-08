ad920d4bd4eaa6974a2316d3c846daff79b9b128
Javier Aguilarte <javier20450@gmail.com>
2025-05-08 18:22:02 -0500

    la edad se pregunta perfetamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx'; // ✅ Correcto
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+],
+providers: [
+  { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ScreenOrientation,
+   // ✅ Aquí debe ir solo, en su propia línea
+],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 import { StorageService } from '../services/storage.service';
-
+import { Router } from '@angular/router';
+import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +12,29 @@ import { StorageService } from '../services/storage.service';
   standalone: false,
 })
 export class HomePage {
+  selectedAge: number = 1;
+  ages: number[] = [];
 
-  constructor(private storage: StorageService)
-   {}
-
-
-  async guardarEdad() {
-    await this.storage.set('edad', 5);
+  constructor() {
+    this.populateAges();
   }
-  
-  async cargarEdad() {
-    const edad = await this.storage.get<number>('edad');
-    console.log('Edad cargada:', edad);
+
+  populateAges() {
+    this.ages = Array.from({ length: 100 }, (_, i) => i + 1);
+  }
+
+  increaseAge() {
+    const currentIndex = this.ages.indexOf(this.selectedAge);
+    if (currentIndex < this.ages.length - 1) {
+      this.selectedAge = this.ages[currentIndex + 1];
+    }
+  }
+
+  decreaseAge() {
+    const currentIndex = this.ages.indexOf(this.selectedAge);
+    if (currentIndex > 0) {
+      this.selectedAge = this.ages[currentIndex - 1];
+    }
   }
 
 }
